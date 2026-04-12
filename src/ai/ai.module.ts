fe577284684a05be22e5b13357bcbb9002a0ed1f
dongyuekai <457545648@qq.com>
2026-04-12 21:20:01 +0800

    first commit

---
+++ src/ai/ai.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AiService } from './ai.service';
+import { AiController } from './ai.controller';
+import { ConfigService } from '@nestjs/config';
+import { ChatOpenAI } from '@langchain/openai';
+
+@Module({
+  controllers: [AiController],
+  providers: [
+    AiService,
+    {
+      provide: 'CHAT_MODEL',
+      useFactory: (configService: ConfigService) => {
+        const modelName =
+          configService.get<string>('OPENAI_MODEL') ||
+          configService.get<string>('OPENAI_MODEL') ||
+          'qwen3.5-plus';
+
+        return new ChatOpenAI({
+          model: modelName,
+          apiKey: configService.get('OPENAI_API_KEY'),
+          configuration: {
+            baseURL: configService.get('OPENAI_BASE_URL'),
+          },
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+})
+export class AiModule {}
---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AiModule } from './ai/ai.module';
+import { ConfigModule } from '@nestjs/config';
+import { SpeechModule } from './speech/speech.module';
+
+@Module({
+  imports: [
+    AiModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    SpeechModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
